d95b6bbdf124b01047a2172ef1ab7f4cd0575862
ChamithMendis <chamith109mendes@gmail.com>
2025-07-22 00:06:50 +0300

    seafarers registerd by month chart data get

--- src/main/java/com/bit/backend/repositories/SeafarersRepository.java
+++ src/main/java/com/bit/backend/repositories/SeafarersRepository.java
@@ -3,9 +3,14 @@ package com.bit.backend.repositories;
 import com.bit.backend.entities.SeafarersEntity;
 import com.bit.backend.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public interface SeafarersRepository extends JpaRepository<SeafarersEntity, Long> {
     Optional<SeafarersEntity> findBySidNo(String sidNo);
+    @Query(nativeQuery = true, value = "select DATE_FORMAT(applied_date, '%Y-%m') as month, count(*) as cnt from ems.seafarers_registration group by month")
+    List<Map<String, Object>> getSeafarersRegisteredByMonth();
 }
