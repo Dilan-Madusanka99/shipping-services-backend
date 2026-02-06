80b3686f579a01a7c3480b3082be7a9dae0b3e05
Dilan Madusanka <dilanmadushanka800@gmail.com>
2026-02-06 11:02:39 +0530

    Update CrewComplaintsRepository.java

--- src/main/java/com/bit/backend/repositories/CrewComplaintsRepository.java
+++ src/main/java/com/bit/backend/repositories/CrewComplaintsRepository.java
@@ -4,4 +4,5 @@ import com.bit.backend.entities.CrewComplaintsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CrewComplaintsRepository extends JpaRepository<CrewComplaintsEntity, Long> {
+  Optional<List<CrewComplaintsEntity>> findBySidNo(String sidNo);
 }
