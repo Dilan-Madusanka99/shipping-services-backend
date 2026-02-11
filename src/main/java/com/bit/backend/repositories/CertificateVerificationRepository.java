fd996d1ede2aca65867400d496f9b2d71b48f5db
Dilan Madusanka <dilanmadushanka800@gmail.com>
2026-02-11 10:25:31 +0530

    Update CertificateVerificationRepository.java

--- src/main/java/com/bit/backend/repositories/CertificateVerificationRepository.java
+++ src/main/java/com/bit/backend/repositories/CertificateVerificationRepository.java
@@ -4,4 +4,6 @@ import com.bit.backend.entities.CertificateVerificationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CertificateVerificationRepository extends JpaRepository<CertificateVerificationEntity, Long> {
+    Optional<List<CertificateVerificationEntity>> findBySidNo(String sidNo);
 }
+
