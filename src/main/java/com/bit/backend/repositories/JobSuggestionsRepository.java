0cb4882aa173bf31355ccf9cd073a701b1c05a69
Chamith Mendis <m.chamith@directfn.com>
2026-05-19 22:05:39 +0530

    get job suggestions from backend

--- src/main/java/com/bit/backend/repositories/JobSuggestionsRepository.java
+++ src/main/java/com/bit/backend/repositories/JobSuggestionsRepository.java
@@ -2,9 +2,20 @@ package com.bit.backend.repositories;
 
 import com.bit.backend.entities.JobSuggestionsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface JobSuggestionsRepository extends JpaRepository<JobSuggestionsEntity, Long> {
     Optional<JobSuggestionsEntity> findByJobIdAndSeafarerId(Long jobId, String seafarerId);
+
+
+    @Query(nativeQuery = true, value = "select sf.id, job.seafarer_no, sf.surname, sf.other_names, sf.position, " +
+            "sf.mobile, sf.email from ems.job_suggestions job\n" +
+            "join ems.seafarers_registration sf\n" +
+            "ON job.seafarer_id = sf.id\n" +
+            "where job.job_id = :jobId")
+    Optional<List<Integer>> findJobSuggestionDetailsById(@Param("jobId") long jobId);
 }
